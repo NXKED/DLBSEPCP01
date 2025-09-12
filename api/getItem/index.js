e1870ac88ccb71a51c839a9a189dcbe73ea61d73
NXKED <nikbreiter@gmx.de>
2025-09-12 11:06:31 +0200

    test-db entries

--- api/getItem/index.js
+++ api/getItem/index.js
@@ -1,51 +1,63 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb')
 
-let cachedClient = null;
-let cachedDb = null;
+let cachedClient = null
+let cachedDb = null
 
 module.exports = async function (context, req) {
   try {
-    context.log("Starting function...");
+    context.log('Starting function...')
 
-    const mongoUrl = process.env.MONGO_URL;
-    if (!mongoUrl) throw new Error("MongoDB env variable not set");
+    const mongoUrl = process.env.MONGO_URL
+    if (!mongoUrl) throw new Error('MongoDB env variable not set')
 
     if (!cachedClient || !cachedDb) {
-      context.log("Kein cached client - Verbinden zur MongoDB...");
+      context.log('Kein cached client - Verbinden zur MongoDB...')
       cachedClient = new MongoClient(mongoUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      });
+      })
 
-      await cachedClient.connect();
-      cachedDb = cachedClient.db("webappdb");
-      context.log("Verbunden zur cached DB");
+      await cachedClient.connect()
+      cachedDb = cachedClient.db('webappdb')
+      context.log('Verbunden zur cached DB')
     } else {
-      context.log("Verwende Cached MongoDB");
+      context.log('Verwende Cached MongoDB')
     }
 
     // Suche nach titel in DB
-    const title = req.query.title;
-    const query = title ? { title: { $regex: title, $options: "i" } } : {};
+    const title = req.query.title
+    const query = title ? { title: { $regex: title, $options: 'i' } } : {}
 
-    const items = await cachedDb.collection("items").find(query).toArray();
+    const items = await cachedDb.collection('items').find(query).toArray()
 
-    context.log(`Fetched ${items.length} items`);
+    // Wenn DB noch nicht mit Daten befuellt
+    if (items.length === 0 && !title) {
+      items = [
+        { title: 'Apfel', description: 'Das ist ein Apfel' },
+        { title: 'Banane', description: 'Das ist eine Banane' },
+        { title: 'Nektarine', description: 'Das ist eine Nektarine' },
+      ]
+      context.log(
+        'Keine Eintraege in der DB gefunden - Beispieldaten fuer Testzwecke'
+      )
+    }
+
+    context.log(`Fetched ${items.length} items`)
 
     context.res = {
       status: 200,
       body: items,
-    };
+    }
   } catch (err) {
-    context.log.error("Error caught", err);
+    context.log.error('Error caught', err)
     context.res = {
       status: 500,
       body: {
-        message: "Function error",
+        message: 'Function error',
         error: err.message,
         stack: err.stack,
       },
-      headers: { "Content-Type": "application/json" },
-    };
+      headers: { 'Content-Type': 'application/json' },
+    }
   }
-};
+}
